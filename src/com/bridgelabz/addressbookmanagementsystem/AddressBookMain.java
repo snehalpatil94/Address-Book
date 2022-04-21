444b18677228fa342ca8971ad1fc53126be87b3f
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-21 22:29:30 +0530

    Welcome Message

---
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbookmanagementsystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+        System.out.println("----------------Welcome to Address Book Management System !-------------------");
+
+    }
+}
