2295cfccc35b102bce6326de132b4b5360c352ef
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-21 22:49:49 +0530

    Ability to create contact in Address Book

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.addressbookmanagementsystem;
 
+/**
+ * Ability to create a Contacts in Address Book with first and last names, address, city, state, zip,
+ * phone number and email
+ *
+ * @author : Snehal Patil
+ */
+
 public class AddressBookMain {
     public static void main(String[] args) {
 
-        System.out.println("----------------Welcome to Address Book Management System !-------------------");
+        System.out.println("------------------Welcome to Address Book Management System !---------------------");
 
     }
 }
