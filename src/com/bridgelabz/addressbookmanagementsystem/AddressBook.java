82faca1ed47cbb54c4a36aa9984fcc043663da0e
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-21 23:20:46 +0530

    Refactor UC2 added funtion to edit contact

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
@@ -9,58 +9,115 @@ package com.bridgelabz.addressbookmanagementsystem;
 import java.util.Scanner;
 
 public class AddressBook {
-    Contacts contacts;
+    Contacts contact;
     Scanner sc = new Scanner(System.in);
 
-    // Method to add new contact in address book
-    public void addContact() {
-        contacts = new Contacts();
-        System.out.println("Enter the first name : ");
-        String firstName = sc.nextLine();
-        System.out.println("Enter the last name : ");
-        String lastName = sc.nextLine();
-        System.out.println("Enter the address : ");
-        String address = sc.nextLine();
-        System.out.println("Enter the city : ");
-        String city = sc.nextLine();
-        System.out.println("Enter the state : ");
-        String state = sc.nextLine();
-        System.out.println("Enter the pincode : ");
-        String pinCode = sc.nextLine();
-        System.out.println("Enter the mobile no : ");
-        String mobileNo = sc.nextLine();
-        System.out.println("Enter Email id : ");
-        String email = sc.nextLine();
-
-        contacts.setfirstName(firstName);
-        contacts.setlastName(lastName);
-        contacts.setAddress(address);
-        contacts.setCity(city);
-        contacts.setState(state);
-        contacts.setPinCode(pinCode);
-        contacts.setMobileNo(mobileNo);
-        contacts.setEmail(email);
-        System.out.println(contacts);
+    /*
+     * To take choice of operation from user to perform in address book
+     */
+    public void option() {
+        Scanner sc = new Scanner(System.in);
+        int option;
+        boolean check = false;
+        do {
+            System.out.println("Press\n1. To add a new contact \n2. To Display contact \n3. To Edit \n4. To Exit");
+            option = sc.nextInt();
+            sc.nextLine();
+            switch (option) {
+                case 1:
+                    addContact();
+                    break;
+                case 2:
+                    displayContact();
+                    break;
+                case 3:
+                    editContact();
+                    break;
+                case 4:
+                    check = true;
+                    System.out.println("Thank You");
+                    break;
+                default:
+                    System.out.println("Please choose a valid option");
+                    break;
+            }
+            System.out.println("------------------------------------------------------------------------------");
+        }
+        while (!check);
     }
 
-    // Displaying added contact in Address Book
+    /*
+     * Taking input from console to add contact in address book
+     */
+    public void addContact() {
+        contact = new Contacts();
+
+        System.out.println("Enter the first name : ");
+        String firstName = this.sc.nextLine();
+        System.out.println("Enter the last name : ");
+        String lastName = this.sc.nextLine();
+        System.out.println("Enter the address : ");
+        String address = this.sc.nextLine();
+        System.out.println("Enter the city : ");
+        String city = this.sc.nextLine();
+        System.out.println("Enter the state : ");
+        String state = this.sc.nextLine();
+        System.out.println("Enter the pincode : ");
+        String pinCode = this.sc.nextLine();
+        System.out.println("Enter the mobile no : ");
+        String mobileNo = this.sc.nextLine();
+        System.out.println("Enter Email id : ");
+        String email = this.sc.nextLine();
+
+        contact.setfirstName(firstName);
+        contact.setlastName(lastName);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setPinCode(pinCode);
+        contact.setMobileNo(mobileNo);
+        contact.setEmail(email);
+        System.out.println(contact);
+        System.out.println();
+    }
+
+    /*
+     * Displaying added contact of address book
+     */
     public void displayContact() {
-        System.out.println("Your contact details are : ");
-        String firstName1 = contacts.getfirstName();
+        String firstName1 = contact.getfirstName();
         System.out.println("First Name : " + firstName1);
-        String lastName1 = contacts.getlastName();
+        String lastName1 = contact.getlastName();
         System.out.println("Last Name : " + lastName1);
-        String address1 = contacts.getAddress();
+        String address1 = contact.getAddress();
         System.out.println("Address : " + address1);
-        String city1 = contacts.getCity();
+        String city1 = contact.getCity();
         System.out.println("City : " + city1);
-        String state1 = contacts.getState();
+        String state1 = contact.getState();
         System.out.println("State : " + state1);
-        String pinCode1 = contacts.getPinCode();
+        String pinCode1 = contact.getPinCode();
         System.out.println("Pin Code : " + pinCode1);
-        String mobileNo1 = contacts.getMobileNo();
+        String mobileNo1 = contact.getMobileNo();
         System.out.println("Mobile No : " + mobileNo1);
-        String email1 = contacts.getEmail();
+        String email1 = contact.getEmail();
         System.out.println("Email id : " + email1);
+        System.out.println();
+    }
+
+    /*
+     * Method to edit contacts of address book
+     */
+    public void editContact() {
+        System.out.println("Please Enter original first name to edit : ");
+        String editName = sc.nextLine();
+
+        if (editName.equalsIgnoreCase(contact.getfirstName())) {
+            addContact();
+        } else {
+            System.out.println("Invalid first name");
+            System.out.println("Please Enter valid first name");
+            System.out.println();
+            editContact();
+        }
     }
 }
