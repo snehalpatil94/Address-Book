737f918dd3994732f4a39d28a8753c47dd6a854d
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-22 00:20:40 +0530

    Ability to add multiple person details to Address Book

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -1,20 +1,20 @@
 package com.bridgelabz.addressbookmanagementsystem;
 
 /**
- * Ability to delete Contact from Address Book
- *
+ * Ability to add multiple person details to Address Book
+ *1
  * @author : Snehal Patil
  */
 
 public class AddressBookMain {
     public static void main(String[] args) {
 
-        System.out.println("----------------Welcome to Address Book Management System !------------------");
+        System.out.println("------------------ Welcome to Address Book Management System !-------------------");
         System.out.println();
 
         AddressBook addressBook = new AddressBook();
 
-        // Calling option method to take choice of add, edit, delete or display
-        addressBook.option();
+        // Calling addressBookMenu to perform various operations in address book
+        addressBook.addressBookMenu();
     }
 }
