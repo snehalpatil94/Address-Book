a7c0581beaa011865d2d53a901fbe77c4a22f991
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-21 23:35:36 +0530

    Ability to delete contact from Address Book

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.addressbookmanagementsystem;
 
 /**
- * Ability to edit Contact in Address Book
+ * Ability to delete Contact from Address Book
  *
  * @author : Snehal Patil
  */
@@ -9,12 +9,12 @@ package com.bridgelabz.addressbookmanagementsystem;
 public class AddressBookMain {
     public static void main(String[] args) {
 
-        System.out.println("------------------Welcome to Address Book Management System !---------------------");
+        System.out.println("----------------Welcome to Address Book Management System !------------------");
         System.out.println();
 
         AddressBook addressBook = new AddressBook();
 
-        // Calling option method to take choice of add, edit or display
+        // Calling option method to take choice of add, edit, delete or display
         addressBook.option();
     }
 }
