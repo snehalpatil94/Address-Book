a1d456a9d3dd511195da9b312c862b23cf273643
snehalpatil94 <erpatilsnehal@gmail.com>
2022-05-08 00:55:48 +0530

    Funtions to add multiple Address Book in Directory

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookDirectory.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookDirectory.java
@@ -1,2 +1,74 @@
-package com.bridgelabz.addressbookmanagementsystem;public class AddressBookDirectory {
+package com.bridgelabz.addressbookmanagementsystem;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Address book Directory functions
+ *
+ * @author : Snehal Patil
+ */
+
+public class AddressBookDirectory {
+
+    static Map<String, AddressBook> addressBookDirctoryMap = new HashMap<>();
+
+    AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+
+    // Getter and Setter for Address Book Directory
+    public static Map<String, AddressBook> getAddressBookMap() {
+        return addressBookDirctoryMap;
+    }
+
+    public static void setAddressBookMap(Map<String, AddressBook> addressBookMap) {
+        AddressBookDirectory.addressBookDirctoryMap = addressBookMap;
+    }
+
+    static Scanner sc = new Scanner(System.in);
+
+    // Method to add Address Book in Directory
+    public static void addNewAddressBook() {
+        System.out.println("Enter the Address Book Name :");
+        String addressbookName = sc.next();
+        if (addressBookDirctoryMap.containsKey(addressbookName)) {
+            System.out.println("This AddressBook is Already Available !");
+        } else {
+            AddressBook addressBook = new AddressBook();
+            addressBookDirctoryMap.put(addressbookName, addressBook);
+        }
+    }
+
+    // Method to Display Address Books present in Directory
+    public static void displayAddressBooks() {
+        System.out.println("Address Books in Directory : ");
+        for (String key : addressBookDirctoryMap.keySet()) {
+            System.out.println(key);
+        }
+    }
+
+    // Method to Edit Address Book
+    public static void editAddressBook() {
+        displayAddressBooks();
+        System.out.println("Enter the Address Book Name :");
+        String addressbookName = sc.next();
+        if (addressBookDirctoryMap.containsKey(addressbookName)) {
+            addressBookDirctoryMap.get(addressbookName).getmenu(addressBookDirctoryMap.get(addressbookName));
+        } else {
+            System.out.println("Entered Address Book Name is Invalid !");
+        }
+    }
+
+    // Method to Delete Address Book
+    public static void deleteAddressBook() {
+        displayAddressBooks();
+        System.out.println("Enter the Address Book Name :");
+        String addressbookName = sc.next();
+        if (addressBookDirctoryMap.containsKey(addressbookName)) {
+            addressBookDirctoryMap.remove(addressbookName);
+            System.out.println("Address Book is Deleted !");
+        } else {
+            System.out.println("Entered Address Book Name is Invalid !");
+        }
+    }
 }
