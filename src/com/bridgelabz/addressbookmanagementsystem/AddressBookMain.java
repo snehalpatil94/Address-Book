8bdd73c2a7af4f0bb51bcda5718752fea69234b0
snehalpatil94 <erpatilsnehal@gmail.com>
2022-05-08 00:56:46 +0530

    Program to add multiple Address Book to the System.

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -1,20 +1,52 @@
 package com.bridgelabz.addressbookmanagementsystem;
 
+import java.util.Scanner;
+
 /**
- * Ability to add multiple person details to Address Book
- *1
+ * Program to add multiple Address Book to the System. Each Address Book has a unique Name - Use Console to add new
+ * Address Book
+ *
  * @author : Snehal Patil
  */
 
 public class AddressBookMain {
+
     public static void main(String[] args) {
-
-        System.out.println("------------------ Welcome to Address Book Management System !-------------------");
+        System.out.println("-----------------Welcome to Address Book Program --------------------");
         System.out.println();
+        Scanner sc = new Scanner(System.in);
 
-        AddressBook addressBook = new AddressBook();
+        String choice;
+        boolean check = false;
+        do {
+            System.out.println(" ADDRESS BOOK SYSTEM MENU : ");
+            System.out.println("1. ADD NEW ADDRESSBOOK \n2. EDIT ADDRESSBOOK \n3. DELETE ADDRESSBOOK" +
+                    "\n4. DISPLAY ADDRESSBOOKS \n5. EXIT");
+            System.out.println("Please Select the Operation Number : ");
+            choice = sc.next();
 
-        // Calling addressBookMenu to perform various operations in address book
-        addressBook.addressBookMenu();
+            switch (choice) {
+                case "1":
+                    AddressBookDirectory.addNewAddressBook();
+                    break;
+                case "2":
+                    AddressBookDirectory.editAddressBook();
+                    break;
+                case "3":
+                    AddressBookDirectory.deleteAddressBook();
+                    break;
+                case "4":
+                    AddressBookDirectory.displayAddressBooks();
+                    break;
+                case "5":
+                    System.out.println("Thank You for using Address Book System.");
+                    check = true;
+                    break;
+                default:
+                    System.out.println("Please Select Valid Option");
+                    break;
+            }
+            System.out.println("---------------------------------------------------------------------");
+        } while (!check);
     }
 }
