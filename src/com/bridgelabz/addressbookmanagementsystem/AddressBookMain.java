70bbab2160df6f11dc734cb3c8abec6644a93b17
snehalpatil94 <erpatilsnehal@gmail.com>
2022-05-09 14:42:31 +0530

    Program to search Person in a City or State across the multiple AddressBook

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -1,51 +1,18 @@
 package com.bridgelabz.addressbookmanagementsystem;
 
-import java.util.Scanner;
-
 /**
- * Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book
+ * Program to search Person in a City or State across the multiple AddressBook .
  *
  * @author : Snehal Patil
  */
 
 public class AddressBookMain {
-
     public static void main(String[] args) {
-        System.out.println("-----------------Welcome to Address Book Program --------------------");
+        System.out.println("-----------------Welcome to address book  Program --------------------");
         System.out.println();
-        Scanner sc = new Scanner(System.in);
 
-        String choice;
-        boolean check = false;
-        do {
-            System.out.println(" ADDRESS BOOK SYSTEM MENU : ");
-            System.out.println("1. ADD NEW ADDRESSBOOK \n2. EDIT ADDRESSBOOK \n3. DELETE ADDRESSBOOK" +
-                    "\n4. DISPLAY ADDRESSBOOKS \n5. EXIT");
-            System.out.println("Please Select the Operation Number : ");
-            choice = sc.next();
-
-            switch (choice) {
-                case "1":
-                    AddressBookDirectory.addNewAddressBook();
-                    break;
-                case "2":
-                    AddressBookDirectory.editAddressBook();
-                    break;
-                case "3":
-                    AddressBookDirectory.deleteAddressBook();
-                    break;
-                case "4":
-                    AddressBookDirectory.displayAddressBooks();
-                    break;
-                case "5":
-                    System.out.println("Thank You for using Address Book System.");
-                    check = true;
-                    break;
-                default:
-                    System.out.println("Please Select Valid Option");
-                    break;
-            }
-            System.out.println("---------------------------------------------------------------------");
-        } while (!check);
+        // Calling directoryMenu method to perform user choice of operation
+        AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+        addressBookDirectory.directoryMenu();
     }
 }
